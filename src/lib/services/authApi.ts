89a632c94108e0bdcadc304c00e24e6e284dcbf4
Lucas Castelano dos Santos <lucas.castelano@radixeng.com>
2024-08-24 23:40:45 -0300

    fix: treat response

--- src/lib/services/authApi.ts
+++ src/lib/services/authApi.ts
@@ -1,17 +1,19 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-import type { User, signinParams } from "../types/authTypes";
+import type { signinParams } from "../types/authTypes";
 
 // Define a service using a base URL and expected endpoints
 export const authApi = createApi({
   reducerPath: "authApi",
   baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:3000" }),
   endpoints: (builder) => ({
-    postSignin: builder.mutation<User, signinParams>({
+    postSignin: builder.mutation<string, signinParams>({
       query: ({ email, password }) => ({
         url: "/signin",
         method: "POST",
         body: { email, password },
       }),
+      transformResponse: (response) => JSON.stringify(response),
+      transformErrorResponse: (error) => error.data,
     }),
   }),
 });
